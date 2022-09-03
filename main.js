1d29b62e3db567514d29c993d87e200fa9d90892
Kaiserkrow <42108060+Kaiserkrow@users.noreply.github.com>
2022-09-03 08:53:42 +0800

    added color changing text based on color brightness

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ function start() {
   let duration = document.getElementById("duration").value;
   let startBtn = document.getElementById("start");
   let colorChangeTime = document.getElementById("rgb-duration").value;
+  let allText = document.getElementsByClassName("text");
+  console.log(allText);
   if (duration == "" || colorChangeTime == "") {
     alert("An input cannot be empty");
   } else if (duration < 0 || colorChangeTime < 0) {
@@ -26,6 +28,17 @@ function start() {
       let b = Math.floor(Math.random() * 255).toString(10);
       document.body.style.backgroundColor = `rgb(${r} ,${g} , ${b})`;
       document.getElementById("color-rgb").textContent = `rgb(${r},${g},${b})`;
+      let luma = 0.2126 * r + 0.7152 * g + 0.0722 * b; // per ITU-R BT.709
+
+      if (luma < 100) {
+        for (let i = 0; i < allText.length; i++) {
+          allText[i].style.color = "white";
+        }
+      } else {
+        for (let i = 0; i < allText.length; i++) {
+          allText[i].style.color = "black";
+        }
+      }
 
       if (duration < 0) {
         clearInterval(colorChange);
